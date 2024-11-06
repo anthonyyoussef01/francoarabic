c3db19bc6cbf6b05320cc67ee4c5fd08d6656f75
anthonyyoussef01 <anthonyyoussef01@gmail.com>
2024-11-06 03:16:42 +0000

    Styling Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 md:p-24 sm:p-12">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className="text-4xl font-bold tracking-tight text-gray-300 sm:text-6xl">
           Franco-Arabic: The Language of Digital Arabia
